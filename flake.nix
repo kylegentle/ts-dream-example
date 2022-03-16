e7671bc57fdaa4b3cc046f10d54e7320d6d526ec
Taylor <tgunnoe@gnu.lv>
2022-03-15 23:32:44 -0400

    Add ts-node and typescript to a shell

--- flake.nix
+++ flake.nix
@@ -3,15 +3,46 @@
 
   inputs = {
     dream2nix.url = "github:nix-community/dream2nix";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
   };
 
-  outputs = { self, dream2nix }@inputs:
+  outputs = { self, dream2nix, nixpkgs }@inputs:
     let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
       dream2nix = inputs.dream2nix.lib2.init {
-        systems = [ "x86_64-linux" ];
+        pkgs = pkgs;
         config.projectRoot = ./.;
       };
-    in dream2nix.makeFlakeOutputs {
-      source = ./.;
-    };
+    in
+      pkgs.lib.recursiveUpdate
+      (dream2nix.makeFlakeOutputs {
+        source = ./.;
+        packageOverrides = {
+          ts-dream-example = {
+            add-inputs = {
+              buildInputs = old: old ++ [
+                #pkgs.nodePackages.typescript
+              ];
+            };
+            install-symlinks = {
+              installPhase = ''
+                mkdir -p $out/bin
+                ln -s $out/lib/node_modules/ts-dream-example/node_modules/ts-node/dist $out/dist
+                ln -s $out/dist/bin.js $out/bin/ts-node
+              '';
+            };
+          };
+        };
+      })
+        {
+          devShells."x86_64-linux".ts-dream-example = pkgs.mkShell {
+            buildInputs = [ pkgs.nodePackages.typescript ];
+            shellHook = ''
+              export PATH="${self.packages."x86_64-linux".ts-dream-example}/bin/:$PATH"
+            '';
+          };
+        };
 }
